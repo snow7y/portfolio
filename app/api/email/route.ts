bc04bad2087a77885220cba184961903e0beb1d0
snow7y <ktc2243020002@edu.kyoto-tech.ac.jp>
2024-09-19 05:51:48 +0900

    serviceをgmailに変更

--- app/api/email/route.ts
+++ app/api/email/route.ts
@@ -7,20 +7,29 @@ export async function POST(request: NextRequest) {
   const {name, email, message} = await request.json();
 
   const transport = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 587,
-    secure: true,
+    service: "gmail",
     auth: {
       user: process.env.NODEMAILER_EMAIL,
       pass: process.env.NODEMAILER_PASSWORD,
     },
+    logger: true,
+    debug: true,
+    connectionTimeout: 30000,
+    socketTimeout: 30000,
   });
 
   const mailOptions: Mail.Options = {
     from: email,
     to: env.NODEMAILER_EMAIL,
-    subject: `portfolio お問い合わせ ${name}様より, ${email}`,
-    text: message,
+    subject: `portfolio お問い合わせ ${name}様より`,
+      html: `
+        <p>【名前】</p>
+        <p>${name}</p>
+        <p>【メッセージ内容】</p>
+        <p>${message}</p>
+        <p>【メールアドレス】</p>
+        <p>${email}</p>
+    `,
   };
 
   try {
